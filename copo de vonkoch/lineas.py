5d24e74f857afbe01db957c69d17bfe7abb45640
Pablo Guallichico <pablomp250@gmail.com>
2018-09-13 14:11:40 -0500

    Correccion del grafico y presentación

--- copo de vonkoch/lineas.py
+++ copo de vonkoch/lineas.py
@@ -7,14 +7,14 @@ from OpenGL.GLU import *
 
 
 def copoVonkoch(lado,n):
-	x_vertice1 = 0
-	y_vertice1 = 0
+	x_vertice1 = 400
+	y_vertice1 = 300-(lado/2)
 
-	x_vertice2 = lado * cos(2*pi/3)
-	y_vertice2 = lado * sin(2*pi/3)
+	x_vertice2 = lado * cos(2*pi/3) + 400
+	y_vertice2 = lado * sin(2*pi/3) + 300-(lado/2)
 
-	x_vertice3 = lado * cos(pi/3)
-	y_vertice3 = lado * sin(pi/3)
+	x_vertice3 = lado * cos(pi/3) + 400
+	y_vertice3 = lado * sin(pi/3) + 300-(lado/2)
 
 	curvaVonkoch(x_vertice1,y_vertice1,x_vertice2,y_vertice2,n)
 	curvaVonkoch(x_vertice2,y_vertice2,x_vertice3,y_vertice3,n)
@@ -26,10 +26,7 @@ def copoVonkoch(lado,n):
 
 def curvaVonkoch(xi,yi,xf,yf,n):
 	if n ==0:
-		glBegin(GL_LINES)
-		glVertex2fv((xi,yi))
-		glVertex2fv((xf,yf))
-		glEnd()		
+		pygame.draw.line(ventana,pygame.Color(70,80,150),(xi,yi),(xf,yf))
 	elif n>0:
 		x1 = xi + (xf - xi) / 3.0
 		y1 = yi + (yf - yi) / 3.0
@@ -49,17 +46,16 @@ def curvaVonkoch(xi,yi,xf,yf,n):
 
 pygame.init()
 display=(800,600)
-pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
-gluPerspective(45,(display[0]/display[1]),0.1,50)
-glTranslatef(0.0,0.0,-50)
-glRotatef(0,0,0,0)
+ventana=pygame.display.set_mode(display)
+
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			quit()
-
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-	copoVonkoch(15,4)
-	pygame.display.flip()
-	pygame.time.wait(10)
+	for i in range(7):
+		ventana.fill((0,0,0))
+		copoVonkoch(450,i)
+		pygame.display.update()
+		pygame.time.wait(1000)
+	
